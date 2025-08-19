6df5a2940967d270785ed068768e97d8334e0e53
Marko Tintor <marko.tintor.18@singimail.rs>
2025-08-19 20:45:02 +0200

    added footer and header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import "./App.css";
 import axios from "axios";
 import { useState } from "react";
 import Movies from "./Components/movies";
-
+import Navigation from "./Templates/Snippets/Navigation";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./Templates/Snippets/Footer";
 const App = () => {
   return (
     <>
+      <Navigation />
       <Movies />
+      <Footer />
     </>
   );
 };
---
+++ src/Templates/Snippets/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="d-flex justify-content-center align-items-center bg-dark text-white p-4">
+      <p className="m-0">Copyright &copy;Mare Movies 2025</p>
+    </footer>
+  );
+};
+
+export default Footer;
